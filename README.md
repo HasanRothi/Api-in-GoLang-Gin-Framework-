efffd1bbe24ba4d9daadec5a3f373461c65863f5
Rothi <hasanrothi99@gmail.com>
2021-01-03 18:24:55 +0600

    basic setup with 2 route & live reload

---
+++ README.md
@@ -0,0 +1,8 @@
+# How to Start
+
+1. [Install go] (https://golang.org/doc/install)
+2. create a folder & go file (main.go)
+3. type go get -u github.com/gin-gonic/gin (install gin framework)
+4. type go get github.com/codegangsta/gin (install live reloader)
+5. copy the code from repo (main.go)
+6. gin run main.go (auto reload while save)
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import "github.com/gin-gonic/gin"
+import "net/http"
+
+func main() {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hi ROtHi..",
+		})
+	})
+	router.GET("/me/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.JSON(200, gin.H{
+			"Welcome": name,
+		})
+	})
+	router.GET("/user/:name/*action", func(c *gin.Context) {
+		name := c.Param("name")
+		action := c.Param("action")
+		message := name + " is " + action
+		c.String(http.StatusOK, message)
+	})
+
+	router.Run() 
+	//without gin reload (go run main.go)
+	// listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	//with gin realod (gin run main.go)
+	//localhost:3000 (default port)
+}
